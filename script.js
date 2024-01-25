32649ea1a5a6b00e7fd33fc7db2fba5556c956ef
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-01-25 08:53:35 +0100

    updated java script file

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function refreshWeather(response) {
 function searchCity(city) {
   let apiKey = "ec4af4682fo7f33ba0a6ate4046d3b06";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(refreshWeather);
 }
-console.log(apiUrl);
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInputElement.value;
-  searchCity(searchInput.value);
+  searchCity(searchInputElement.value);
 }
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
