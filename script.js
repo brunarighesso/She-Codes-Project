e9bfcaca869c7af443287573f4fbc900735e5fea
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-01-25 12:57:06 +0100

    added time function

--- script.js
+++ script.js
@@ -4,13 +4,40 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = formatDate(date);
+  let date = new Date(response.data.time * 1000);
   console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let day = date.detDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
